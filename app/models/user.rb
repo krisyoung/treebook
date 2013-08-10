57d52f50c3d2de057230f4bc9ee3cf92973fc992
Kris Young <krisyoung1980@gmail.com>
2013-08-10 19:25:35 +0100

    styled sign in form

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,15 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :token_authenticatable, :confirmable,
-  # :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+    # Include default devise modules. Others available are:
+    # :token_authenticatable, :confirmable,
+    # :lockable, :timeoutable and :omniauthable
+    devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  # Setup accessible (or protected) attributes for your model
-  attr_accessible :first_name, :last_name, :profile_name, :email, :password, :password_confirmation, :remember_me
-  # attr_accessible :title, :body
+    # Setup accessible (or protected) attributes for your model
+    attr_accessible :first_name, :last_name, :profile_name, :email, :password, :password_confirmation, :remember_me
+    has_many :statuses
+
+    def full_name
+        first_name + " " + last_name
+    end
 end
